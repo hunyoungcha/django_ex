c79f2a9a9b0b1f07a9d52e2abacfbae0b1dc6473
hunyoungcha <chamgf5247@naver.com>
2024-10-21 13:09:40 +0900

    myapp 취약한 페이지 생성 완료

--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,20 @@
 from django.shortcuts import render
+from django.db import connection
+
 
 def index(request):
-    return render(request, 'myapp/index.html')
+    with connection.cursor() as cursor:
+        input = request.GET.get('input')
+        query = f"select * from myapp_py5test where name = '{input}' "
+        cursor.execute(query)
+
+        rows= cursor.fetchall()
+
+        context = {
+            'rows':rows,
+
+        }
+
+        return render(request, 'myapp/index.html', context)
+
+
